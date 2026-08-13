9ab9b5ac76349f898efbe01b9edf6f0fc38e05fd
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-13 21:56:02 +0200

    feat: Simplify email validation method in User class

--- src/pl/tlewandster/ffwork/domain/User.java
+++ src/pl/tlewandster/ffwork/domain/User.java
@@ -1,25 +1,19 @@
 package pl.tlewandster.ffwork.domain;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public abstract class User {
     private final String email;
     private String displayName;
 
     public User(String email) {
-        if (!isValidEmail(email)){
+        if (!isValidEmail(email)) {
             throw new IllegalArgumentException("Invalid email address");
         }
         // TODO Checking the email address in the Users repository
         this.email = email;
     }
 
-    private boolean isValidEmail(String email){
-        Pattern emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
-                Pattern.CASE_INSENSITIVE);
-        Matcher matcher = emailPattern.matcher(email);
-        return matcher.matches();
+    private boolean isValidEmail(String email) {
+        return email.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
     }
 
     //TODO Add the necessary accessors
