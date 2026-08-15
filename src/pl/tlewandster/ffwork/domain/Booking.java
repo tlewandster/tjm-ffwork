6657362ea3745f9355bd8c39e2b914157c0dc59a
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-15 18:48:09 +0200

    feat: Add BookingRepository interface and InMemoryBookingRepository implementation; update Booking class to expose durationMinutes method

--- src/pl/tlewandster/ffwork/domain/Booking.java
+++ src/pl/tlewandster/ffwork/domain/Booking.java
@@ -7,19 +7,27 @@ import java.time.LocalDateTime;
 
 public class Booking {
     private static long bookCounter;
+    protected LocalDateTime start;
+    protected LocalDateTime end;
     private String id;
     private User user;
     private Resource resource;
-    protected LocalDateTime start;
-    protected LocalDateTime end;
     private BookingStatus status;
     private Money calculatedPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
     /* TODO Payment class
     private Payment payment;*/
 
     //TODO Overloaded constructors by factory and invariant validations.
 
-    protected long durationMinutes() {
+    public long durationMinutes() {
         return Duration.between(start, end).toMinutes();
     }
 }
