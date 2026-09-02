8c2191bf050b45f27f84ff3e812134df5ddf833c
Tomasz Lewandowski <tlewandster@gmail.com>
2026-09-03 00:23:05 +0200

    feat: Refactor happy hours discount to use BigDecimal for improved precision

--- src/pl/tlewandster/ffwork/pricing/HappyHoursPricing.java
+++ src/pl/tlewandster/ffwork/pricing/HappyHoursPricing.java
@@ -3,14 +3,16 @@ package pl.tlewandster.ffwork.pricing;
 import pl.tlewandster.ffwork.domain.Booking;
 import pl.tlewandster.ffwork.money.Money;
 
+import java.math.BigDecimal;
+
 public class HappyHoursPricing implements PricingPolicy {
-    private static double happyHoursDiscount = 0.3;
+    private static final BigDecimal happyHoursDiscount = BigDecimal.valueOf(0.3);
 
     @Override
     public Money price(Booking booking) {
         int hourOfStart = booking.getStart().getHour();
         boolean isHappyHours = hourOfStart >= 14 && hourOfStart <= 16;
         return PricingPolicy.super.price(booking).multiply(
-                isHappyHours ? 1 - happyHoursDiscount : 1);
+                isHappyHours ? BigDecimal.ONE.subtract(happyHoursDiscount) : BigDecimal.ONE);
     }
 }
